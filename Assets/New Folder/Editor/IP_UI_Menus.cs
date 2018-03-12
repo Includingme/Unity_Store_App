4b8c10cb7d27451d1b83070b6f1ec4e185ab90db
Romas <romualdas.tamosauskas@gmail.com>
2018-03-12 11:49:05 +0200

    Start at 2018.03.12 11:48

---
+++ Assets/New Folder/Editor/IP_UI_Menus.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Basic.UI
+{
+    public class IP_UI_Menus : MonoBehaviour
+    {
+        [MenuItem("Basic.UI/UI Tools/Create UI Group")]
+        public static void CreateUIGroup()
+        {
+            GameObject uiGroup = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/New Folder/Prefab/UI_GRP.prefab");
+
+                if(uiGroup)
+            {
+                GameObject createdGroup = (GameObject)Instantiate(uiGroup);
+                createdGroup.name = "UI_GRP";
+            }
+                else
+            {
+                EditorUtility.DisplayDialog("UI Tools Warning", "Cannot find UI Group Prefab", "OK");
+            }
+        }
+    }
+}
